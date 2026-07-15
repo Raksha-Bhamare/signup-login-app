aeeec13c02cfc542bccc2117843cb79499c70b18
Raksha Bhamare <rakshabhamare@gmail.com>
2026-07-15 20:12:47 +0530

    Fix API

--- api/index.js
+++ api/index.js
@@ -1,3 +1,56 @@
-const app = require("../backend/server");
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("../backend/db");
+const User = require("../backend/User");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/signup", async (req, res) => {
+  try {
+    await connectDB();
+
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    await new User({ name, email, password }).save();
+
+    res.status(201).json({ message: "Registration successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    await connectDB();
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Email or Password",
+      });
+    }
+
+    res.status(200).json({
+      message: "Login Successful",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 module.exports = app;
